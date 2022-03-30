9bdd1d5e2e39257841a701c4ea89b1b9e41a905b
soumy <soumy@SAUMYADIP>
2022-03-30 19:47:23 +0530

    Add project files.

---
+++ FileHandling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingHandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandling fileHandling = new FileHandling();
+            //fileHandling.WriteFile();
+            Console.WriteLine("Detailed");
+            Console.WriteLine("");
+            fileHandling.ReadFile();
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+            //Console.Read();
+
+            SuccessFileHandling successFile = new SuccessFileHandling();
+            Console.WriteLine("Success");
+            Console.WriteLine("");
+            successFile.ReadFile();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+
+
+            FailedFileHandling failedFile = new FailedFileHandling();
+            Console.WriteLine("Failed");
+            Console.WriteLine("");
+            failedFile.ReadFile();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+
+
+            MissedFileHandling missedFile = new MissedFileHandling();
+            Console.WriteLine("Missed");
+            Console.WriteLine("");
+            missedFile.ReadFile();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+
+
+            DialledFileHandling dialledFile = new DialledFileHandling();
+            Console.WriteLine("Dialled");
+            Console.WriteLine("");
+            dialledFile.ReadFile();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+            Console.Read();
+        }
+    }
+}
